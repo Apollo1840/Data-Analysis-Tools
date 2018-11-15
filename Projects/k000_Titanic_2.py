d1c1ec5a66da85edac32e51e49b3955ba8e561e1
Apollo1840 <zoucongyu1993@hotmail.com>
2018-11-15 10:25:39 +0100

    refine titanic2

--- Projects/k000_Titanic_2.py
+++ Projects/k000_Titanic_2.py
@@ -34,13 +34,26 @@ pylab.rcParams[ 'figure.figsize' ] = 8 , 6
 from ploters import plot_distribution, plot_categories, plot_correlation_map,plot_variable_importance
 
 
-
+DATA_PATH='Projects\\datasets\\k000_titanic\\'
     
     
 # -----------------------------------------------------------------------------    
 # get titanic & test csv files as a DataFrame
-train = pd.read_csv('datasets\\k000_titanic\\train.csv')
-test = pd.read_csv('datasets\\k000_titanic\\test.csv')
+
+'''
+    in k000_Titanic_1, it works on train set and deploy the changes on test data.
+    in this one, it combines train and test first.
+
+'''
+
+import os
+path = os.getcwd()
+while(os.path.basename(path) != 'Data-Analysis-Tools'):
+    path = os.path.dirname
+os.chdir(path)
+    
+train = pd.read_csv(DATA_PATH + 'train.csv')
+test = pd.read_csv(DATA_PATH + 'test.csv')
 
 full = train.append( test , ignore_index = True )
 titanic = full[ :891 ]
@@ -49,6 +62,8 @@ del train , test
 
 print ('Datasets:' , 'full:' , full.shape , 'titanic:' , titanic.shape)
 
+
+# plot some charts
 
 plot_correlation_map( titanic )
 
@@ -59,23 +74,17 @@ plot_distribution( titanic , var = 'Age' , target = 'Survived' , row = 'Sex' )
 plot_categories( titanic , cat = 'Embarked' , target = 'Survived' )
 
 
+'''
+    here it is going to change some attributes to dummies.
 
-# sex; embarked; pclass ---
+'''
+# --- sex ---
 
 # Transform Sex into binary values 0 and 1
 sex = pd.Series( np.where( full.Sex == 'male' , 1 , 0 ) , name = 'Sex' )
+# sex = full.Sex.map({'male': 1,'female':0})
 
-# Create a new variable for every unique value of Embarked
-embarked = pd.get_dummies( full.Embarked , prefix='Embarked' )
-embarked.head()
-
-# Create a new variable for every unique value of Embarked
-pclass = pd.get_dummies( full.Pclass , prefix='Pclass' )
-pclass.head()
-
-
-
-# Age; Fare ---
+# --- Age; Fare ---
 
 # Create dataset
 imputed = pd.DataFrame()
@@ -89,11 +98,14 @@ imputed[ 'Fare' ] = full.Fare.fillna( full.Fare.mean() )
 imputed.head()
 
 
-# title ---
+# --- title ---
 
 title = pd.DataFrame()
 # we extract the title from each name
+full['Name']
+
 title[ 'Title' ] = full[ 'Name' ].map( lambda name: name.split( ',' )[1].split( '.' )[0].strip() )
+title['Title'].value_counts()
 
 # a map of more aggregated titles
 Title_Dictionary = {
@@ -120,13 +132,8 @@ Title_Dictionary = {
 
 # we map each title
 title[ 'Title' ] = title.Title.map( Title_Dictionary )
-title = pd.get_dummies( title.Title )
-#title = pd.concat( [ title , titles_dummies ] , axis = 1 )
 
-title.head()
-
-
-# cabin ---
+# --- cabin ---
 
 cabin = pd.DataFrame()
 
@@ -134,23 +141,17 @@ cabin = pd.DataFrame()
 cabin[ 'Cabin' ] = full.Cabin.fillna( 'U' )
 
 # mapping each Cabin value with the cabin letter
-cabin[ 'Cabin' ] = cabin[ 'Cabin' ].map( lambda c : c[0] )
-
-# dummy encoding ...
-cabin = pd.get_dummies( cabin['Cabin'] , prefix = 'Cabin' )
-
-cabin.head()
+cabin[ 'Cabin' ] = cabin[ 'Cabin' ].apply( lambda c : c[0] )
 
 
-
-# ticket ---
+# --- ticket ---
+full.Ticket
 
 # a function that extracts each prefix of the ticket, returns 'XXX' if no prefix (i.e the ticket is a digit)
 def cleanTicket( ticket ):
     ticket = ticket.replace( '.' , '' )
     ticket = ticket.replace( '/' , '' )
-    ticket = ticket.split()
-    ticket = map( lambda t : t.strip() , ticket )
+    ticket = [t.strip() for t in ticket.split()]
     ticket = list(filter( lambda t : not t.isdigit() , ticket ))
     if len( ticket ) > 0:
         return ticket[0]
@@ -160,82 +161,116 @@ def cleanTicket( ticket ):
 ticket = pd.DataFrame()
 
 # Extracting dummy variables from tickets:
-ticket[ 'Ticket' ] = full[ 'Ticket' ].map( cleanTicket )
-ticket = pd.get_dummies( ticket[ 'Ticket' ] , prefix = 'Ticket' )
-
-ticket.shape
-ticket.head()
+ticket[ 'Ticket' ] = full[ 'Ticket' ].apply( cleanTicket )
+# ticket[ 'Ticket' ].value_counts()
 
 
-# Family ---
+# -- Family ---
 
 family = pd.DataFrame()
 
 # introducing a new feature : the size of families (including the passenger)
 family[ 'FamilySize' ] = full[ 'Parch' ] + full[ 'SibSp' ] + 1
 
-# introducing other features based on the family size
-family[ 'Family_Single' ] = family[ 'FamilySize' ].map( lambda s : 1 if s == 1 else 0 )
-family[ 'Family_Small' ]  = family[ 'FamilySize' ].map( lambda s : 1 if 2 <= s <= 4 else 0 )
-family[ 'Family_Large' ]  = family[ 'FamilySize' ].map( lambda s : 1 if 5 <= s else 0 )
 
-family.head()
+# --- all ---
 
-# all ---
+# full_X = pd.concat( [ imputed , embarked , cabin , sex ] , axis=1 )
+# full_X.head()
 
-full_X = pd.concat( [ imputed , embarked , cabin , sex ] , axis=1 )
-full_X.head()
+full_X = pd.concat( [ sex, full.Embarked, full.Pclass, imputed,title, cabin, ticket, family] , axis=1 )
+print(full_X.columns)
+print(full_X.head())
+full_X.to_csv(DATA_PATH +'titanic_data2.csv' )
 
+
+embarked = pd.get_dummies( full.Embarked , prefix='Embarked' )
+pclass = pd.get_dummies( full.Pclass , prefix='Pclass' )
+
+ticket = pd.get_dummies( ticket[ 'Ticket' ] , prefix = 'Ticket' )
+cabin = pd.get_dummies( cabin['Cabin'] , prefix = 'Cabin' )
+title = pd.get_dummies( title.Title, prefix='Title')
+
+# (discretelization) introducing other features based on the family size
+family[ 'Family_Single' ] = family[ 'FamilySize' ].apply( lambda s : 1 if s == 1 else 0 )
+family[ 'Family_Small' ]  = family[ 'FamilySize' ].apply( lambda s : 1 if 2 <= s <= 4 else 0 )
+family[ 'Family_Large' ]  = family[ 'FamilySize' ].apply( lambda s : 1 if 5 <= s else 0 )
+
+
+full_X = pd.concat( [ sex, embarked, pclass, imputed ,title, cabin, ticket, family] , axis=1 )
+print(full_X.columns)
+print(full_X.head())
 
 #-------------------------------------------------------------------------------
+# preparation
 
 # Create all datasets that are necessary to train, validate and test models
-train_valid_X = full_X[ 0:891 ]
-train_valid_y = titanic.Survived
+
+'''
+    train_x, train_y is the train data
+    test_x, test_y is the validation data
+    
+    train_all_x, train_all_y is the final training data
+    
+    test_X is the data to predict
+
+'''
+
+
+
+train_X = full_X[ 0:891 ]
+train_Y = titanic.Survived.values.reshape(-1,1)
 test_X = full_X[ 891: ]
-train_X , valid_X , train_y , valid_y = train_test_split( train_valid_X , train_valid_y , train_size = .7 )
+train_x , test_x , train_y , test_y = train_test_split( train_X , train_Y , train_size = .7 )
 
-print (full_X.shape , train_X.shape , valid_X.shape , train_y.shape , valid_y.shape , test_X.shape)
+print(full_X.shape , train_X.shape, train_Y.shape, test_X.shape)
+print(train_x.shape , train_y.shape , test_x.shape, test_y.shape)
 
 
-plot_variable_importance(train_X, train_y)
+# plot_variable_importance(train_X, train_y)
+
+
+# modeling ------------------------------------------------------------------
+
 
 model = RandomForestClassifier(n_estimators=100)
 # model = GradientBoostingClassifier()
 # model = LogisticRegression(C=0.5, penalty='l2', tol=1e-9)
 rfecv = RFECV( estimator = model , step = 1 , cv = StratifiedKFold( train_y , 2 ) , scoring = 'accuracy' )
-rfecv.fit( train_X , train_y )
-print(rfecv.score( train_X , train_y ))
-print(rfecv.score( valid_X , valid_y ))
+rfecv.fit( train_x , train_y )
+print(rfecv.score( train_x , train_y ))
+print(rfecv.score( test_x, test_y ))
 
 
-test_Y = rfecv.predict( test_X )
-passenger_id = full[891:].PassengerId
-
-
-test = pd.DataFrame( { 'PassengerId': passenger_id , 'Survived': test_Y.astype(np.int32) } )
-test = test.reset_index()
-test = test.drop(columns = ['index'])
-test.info()
-
-
-test.shape
-test.head()
-test.to_csv( 'titanic_pred.csv' , index = False )
 
 score = 0
 while(score < 0.78):
     model = MLPClassifier((300,100,50), activation = 'tanh', tol=10e-6)
-    model.fit( train_X , train_y )
-    model.fit( train_X , train_y )
-    model.fit( train_X , train_y )
-    print(model.score( train_X , train_y ))
-    print(model.score( valid_X , valid_y ))
-    score = model.score( valid_X , valid_y )
+    model.fit( train_x , train_y )
+    model.fit( train_x , train_y )
+    model.fit( train_x , train_y )
+    print(model.score( train_x , train_y ))
+    print(model.score( test_x , test_y ))
+    score = model.score( test_x , test_y )
 
-test_Y = model.predict( test_X )
-passenger_id = full[891:].PassengerId
-test = pd.DataFrame( { 'PassengerId': passenger_id , 'Survived': test_Y.astype(np.int32) } )
-test.to_csv( 'titanic_pred.csv' , index = False )
+
+# ------------------------------------------------------------------
+# output
+
+def save_result(model):
+    test_Y = model.predict( test_X )
+    passenger_id = full[891:].PassengerId
+    test = pd.DataFrame( { 'PassengerId': passenger_id , 'Survived': test_Y.astype(np.int32) } )
+    # test = test.reset_index()
+    # test = test.drop(columns = ['index'])
+    print(test.info())
+    test.to_csv( DATA_PATH + 'titanic_pred.csv' , index = False )
+
+
+save_result(rfecv)
+save_result(model)
+
+
+
 
 
