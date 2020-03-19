09526a9744513c45bdd76a4c4fc66f21bbb72395
Apollo1840 <zoucongyu1993@hotmail.com>
2020-03-19 15:07:08 +0100

    add wav ecg

---
+++ datasets/dataframes.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  3 12:11:29 2018
+
+@author: zouco
+"""
+
+import numpy as np
+import pandas as pd
+
+data = {'id': [0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+        'x': [1.2, 2, 3, 4, 5.6, 6, 7, 8, 9.4, 10, 11, 12],
+        'y': [13, 9, 8, 4, np.nan, 0, 5, np.nan, 7, 7, 10, 11],
+        'obj': [np.random.choice(['a', 'b', 'c']) for _ in range(12)]
+        }
+
+df1 = pd.DataFrame(data)
+
+df2 = pd.read_excel('datasets/sample.xlsx')
+
+
+
+
--- projects/MAIS.py
+++ projects/MAIS.py
@@ -1,33 +1,27 @@
-
-
 import pandas as pd
-df=pd.read_excel(r'C:\Users\zouco\Desktop\ALl Collision.xlsx')
-df.shape
-df.columns
+
+df = pd.read_excel(r'C:\Users\zouco\Desktop\ALl Collision.xlsx')
+print(df.shape)
+print(df.columns)
     
 df2 = df[(df.ANZKOLL == 1) & (df.VDI1 != 0) & (df.VDI1 != 99) & (df.VDI3 != 0) & (df.VDI2 != 7)]
-df2.shape
+print(df2.shape)
 len(df2.FALL.unique())
 
 
 # unique_count([1,2,3,4,3,5,6,7,1])
 
 result = df2.groupby('MAIS98')['FALL'].agg('nunique')
-result.index
-result
+print(result.index)
+print(result)
 
 # df2.groupby('FALL')['MAIS98'].agg(unique_count).sort_values(ascending=False)
 
-
 onlyone = lambda x: max(x)
 dfr = df2.groupby('FALL')['MAIS98'].agg(onlyone)
-
-
 
 df3 = df2[df2.MAIS98 == [dfr[i] for i in df2.FALL]]
 
 result = df3.groupby('MAIS98')['FALL'].agg('nunique')
 print(result)
-sum(result)
-
- 
+print(sum(result))
